bdd3999311b7fce0289b563334b0580fb399a971
Darren Gosbell <dgosbell@users.noreply.github.com>
2022-09-02 11:23:52 +1000

    Adding Algolia search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,7 +28,7 @@ const config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
+  
   presets: [
     [
       'classic',
@@ -144,6 +144,29 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'USDO2S63VO',
+  
+        // Public API key: it is safe to commit it
+        apiKey: 'ec3953d03f14df2d21379c9f6dbd1984',
+  
+        indexName: 'daxstudio',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        //externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Algolia search parameters
+        //searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        //searchPagePath: 'search',
+  
+        //... other Algolia params
       },
     }),
   plugins:[
